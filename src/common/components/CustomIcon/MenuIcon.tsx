6ad2d77b8bcf0f931277b9d04e2e0f2077657ab8
Andre H <andrehadiantolesmana@gmail.com>
2024-03-12 22:59:45 +0800

    🐛 fix: linting

--- src/common/components/CustomIcon/MenuIcon.tsx
+++ src/common/components/CustomIcon/MenuIcon.tsx
@@ -2,11 +2,11 @@ import { CustomIcon, type CustomIconProps } from "./CustomIcon";
 
 export const MenuIcon = (props: CustomIconProps) => {
   return (
-    <CustomIcon {...props} strokeWidth={1.5} stroke="currentColor">
+    <CustomIcon {...props} stroke="currentColor" strokeWidth={1.5}>
       <path
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
         strokeLinecap="round"
         strokeLinejoin="round"
-        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
       />
     </CustomIcon>
   );
