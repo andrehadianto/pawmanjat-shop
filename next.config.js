d0e69ae1d86859f85f955bdffde06d8db8663feb
Andre H <andrehadiantolesmana@gmail.com>
2024-03-12 22:48:23 +0800

    Initial commit

--- next.config.js
+++ next.config.js
@@ -5,24 +5,24 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
-  output: "export",
+  output: 'export',
 
   /**
-   * Set base path. This is usually the slug of your repository.
+   * Set base path. This is the slug of your GitHub repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+  basePath: '/pawmanjat-shop',
 
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
    *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
    */
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
---
+++ src/common/components/CustomIcon/index.ts
@@ -0,0 +1,4 @@
+export * from "./MenuIcon";
+export * from "./SpinnerIcon";
+export * from "./TelegramIcon";
+
